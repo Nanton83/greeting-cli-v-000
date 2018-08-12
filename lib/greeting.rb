02e383a69e20826c2824e6764ad18eabea831e1b
Nick Romans <nicholas.romans@gmail.com>
2018-08-12 16:24:45 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,9 @@
 # code the #greeting method here!
 
 def greeting(name)
+<<<<<<< HEAD
   puts "Hello #{name}. It's nice to meet you."
+=======
+  puts "Hello #{name}.  It's nice to meet you."
+>>>>>>> bdee7df9a80c5179ecc0bd40abade0021dac1047
 end
